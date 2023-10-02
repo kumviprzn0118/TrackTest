bfa004e7e0121ae8ce55842cab7b2baa4e0a15ac
km <kumamoto.yusuke@shanon.co.jp>
2023-10-02 16:04:55 +0900

    コード修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,76 @@
 import React, { useState } from "react";
 import './App.css';
 type ButtonProps = {
+  id:string;
   name: string;
   price: number;
   count: number;
-  setCount: React.Dispatch<React.SetStateAction<number>>;
+  setItem: (id: string) => void;
 };
-
-function Button({name,price,count,setCount}: ButtonProps){
+type Item = {
+  id: string;
+  name: string;
+  price: number;
+  count: number;
+};
+function Button({id,name,price,count,setItem}: ButtonProps){
+  const handleButtonClick = () => {
+    setItem(id); // ボタンがクリックされたときにアイテムの id を渡す
+  };
   return (
 <>
 <div className="wrap_btn">
-        <button onClick={() => setCount(count + 1)} className="btn js-btn" id="coffee" >
+        <button onClick={handleButtonClick} className="btn js-btn" id={id} >
           <span className="name">{name}</span><div><span className="js-price">{price}</span>円</div>
         </button>
         <div className="counter" id="coffee-count">{count}</div>
 </div>
 </>  
 )}
-
 function App() {
-  const [coffee_count, setCoffeeCount] = useState<number>(0);
-  const [tea_count, setTeaCount] = useState<number>(0);
-  const [milk_count, setMilkCount] = useState<number>(0);
-  const [coke_count, setCokeCount] = useState<number>(0);
-  const [beer_count, setBeerCount] = useState<number>(0);
-
-  const coffee_price = 480;
-  const tea_price = 280;
-  const milk_price = 180;
-  const coke_price = 190;
-  const beer_price = 580;
-
-  const count = coffee_count + tea_count + milk_count + coke_count + beer_count;
-  const price = coffee_count*coffee_price + tea_count*tea_price + milk_count * milk_price + coke_count * coke_price + beer_count * beer_price ;
-
+  //先にITemを定義する
+  const items: Item[]  = [
+    {id:"1",name:"コーヒー",price:480,count:0},
+    {id:"2",name:"紅茶",price:280,count:0},
+    {id:"3",name:"ミルク",price:180,count:0},
+    {id:"4",name:"コーラ",price:190,count:0},
+    {id:"5",name:"ビール",price:580,count:0}
+  ]
+  //useStateで管理するようにする
+  const [itemsState, setItemsState] = useState<Item[]>(items);
+  //クリックしたときの処理。
+  const handleItemClick = (itemId: string) => {
+    setItemsState((prevItems) => {
+      const updatedItems = prevItems.map((item) => {
+        if (item.id === itemId) {
+          return { ...item, count: item.count + 1 };
+        }
+        return item;
+      });
+      return updatedItems;
+    });
+  };
+ const totalCount = itemsState.reduce((count,item)=>count + item.count,0)
+ const totalPrice = itemsState.reduce((price,item)=>price + item.count*item.price,0)
   return (
     <div className="App">
   <div id="wrap_contents">
     <div id="left-content">
-      <Button name="コーヒー" price={coffee_price} count={coffee_count} setCount={setCoffeeCount}/>
-      <Button name="紅茶" price={tea_price} count={tea_count} setCount={setTeaCount}/>
-      <Button name="ミルク" price={milk_price} count={milk_count} setCount={setMilkCount}/>
-      <Button name="コーラ" price={coke_price} count={coke_count} setCount={setCokeCount}/>
-      <Button name="ビール" price={beer_price} count={beer_count} setCount={setBeerCount}/>
+    {itemsState.map((product, index) => (
+            <Button
+              id = {product.id}
+              name={product.name}
+              price={product.price}
+              count={product.count}
+              setItem={handleItemClick}
+            />
+      ))}
     </div>
     <div id="right-content">
       <div className="wrap_data">
         <h3>お会計</h3>
-        <div>商品数：<span id="count">{count}</span>個</div>
-        <div>合計金額：<span id="price">{price}</span>円</div>
+        <div>商品数：<span id="count">{totalCount}</span>個</div>
+        <div>合計金額：<span id="price">{totalPrice}</span>円</div>
       </div>
     </div>
   </div>
